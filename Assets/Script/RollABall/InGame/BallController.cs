960dd3dabc4b892d97eb9555041a3e53fb7b324e
髙橋月菜 <vga29-ac220041@sankogakuen.jp>
2023-11-15 15:49:33 +0900

    制限時間と邪魔ブロック追加
    *途中でつまずき、中途半端です

---
+++ Assets/Script/RollABall/InGame/BallController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    void Update()
+
+    {
+
+
+        Debug.Log(this.transform.eulerAngles);
+
+
+    }
+
+}
